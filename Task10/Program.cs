e4d7e2609355b4b25d68ba666464c9a58ace2984
Tatiana Maslenkova <tsevastianova21@gmail.com>
2022-09-22 16:10:37 +0300

    Task 10 added

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Math.Abs(int.Parse(Console.ReadLine()!));
+
+if((number <= 99) || (number > 999))
+{
+    Console.WriteLine("Нужно ввести трехзначное число!");
+}
+else
+{
+    Console.WriteLine($"Вторая цифра числа: {number/ 10 % 10}");
+}
