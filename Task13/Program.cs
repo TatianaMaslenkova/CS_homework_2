b6fb0e86d62a6801dd732c282035b35572aff7ca
Tatiana Maslenkova <tsevastianova21@gmail.com>
2022-09-22 17:33:52 +0300

    Task 13 added

---
+++ Task13/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int number = Math.Abs(int.Parse(Console.ReadLine()!));
+int thirdDigit = 0;
+
+if(number < 100)
+{
+    Console.WriteLine("Нет третьей цифры");
+}
+else
+{
+    while(number > 999)
+    {
+        number = number / 10;
+    }
+    thirdDigit = number % 10;
+    Console.WriteLine($"Третья цифра: {thirdDigit}");
+}
+
+// Второй способ - через строку (проверяла, работает):
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int number = Math.Abs(int.Parse(Console.ReadLine()!));
+// string numstring = Convert.ToString(number);
+// if (numstring.Length < 3)
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+// else 
+// {
+//     Console.WriteLine($"Третья цифра: {numstring[2]}");
+// }
