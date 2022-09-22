8914625539c15ddcdd220ad71780c5227737ec18
Tatiana Maslenkova <tsevastianova21@gmail.com>
2022-09-22 16:26:12 +0300

    Task 15 added

---
+++ Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите номер дня недели: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if((number < 1) || (number > 7))
+{
+    Console.WriteLine("Нет такого номера дня недели!");
+}
+else if((number >= 1) && (number <= 5))
+{
+    Console.WriteLine("Это будний день");
+}
+else
+{
+    Console.WriteLine("Это выходной день");
+}
+
